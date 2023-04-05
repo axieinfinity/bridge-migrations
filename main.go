cb676c973790de870c713a2bce579787dec5121c
Bui Quang Minh <minh.bui@skymavis.com>
2023-04-05 13:51:27 +0700

    chore: cleanup old data
    As new code is not backward compatible with old data, we cleanup old data and
    re-run the tracker from recent blocks.

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package migration
 
 import (
+	"strings"
+
 	bridgeCore "github.com/axieinfinity/bridge-core"
 	models20220515 "github.com/axieinfinity/bridge-migrations/20220515_init"
 	models20220701 "github.com/axieinfinity/bridge-migrations/20220701_retry_all_failed_tx"
 	models20220703 "github.com/axieinfinity/bridge-migrations/20220703_add_timestamp_when_submit_tx"
 	models20220707 "github.com/axieinfinity/bridge-migrations/20220707_add_processed_receipt_table"
 	models20220708 "github.com/axieinfinity/bridge-migrations/20220708_update_processed_block"
+	models20230504 "github.com/axieinfinity/bridge-migrations/20230504_drop_old_data"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
-	"strings"
 )
 
 func Migrate(db *gorm.DB, cfg *bridgeCore.Config) error {
@@ -19,6 +21,7 @@ func Migrate(db *gorm.DB, cfg *bridgeCore.Config) error {
 		models20220703.Migrate(),
 		models20220707.Migrate(),
 		models20220708.Migrate(),
+		models20230504.Migrate(),
 	}
 	migrate := gormigrate.New(db, gormigrate.DefaultOptions, migrations)
 	for _, version := range migrations {
